838f9c4a8f2e2450709e640ad13cd814fdec1681
Jasper Davis <jasper.nicholas.davis@gmail.com>
2015-01-25 17:00:11 -0500

    more updates to responsive and overall look of portfolio

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,16 +3,34 @@
 (function($) {
   $(document).ready(function() {
     // Add your jQuery code here
+    $('.main-logo').toggleClass('unloading');
+    $('#main_navigation li').toggleClass('unloading');
+    $('.main-slick').toggleClass('unloading2');
+
 
     $('.mobi_nav_btn').on('click', function(eventObject) {
-    	if ( $('#main_navigation li').css('display') === 'none' ) {
+    	if ( $('#main_navigation li').css('display') == 'none' ) {
 				$('#main_navigation li').slideDown()
+				$(this).toggleClass('mobi_nav_btn_clicked');
 			} 
 			else {
 				$('#main_navigation li').slideUp()
+				$(this).toggleClass('mobi_nav_btn_clicked');
 			}
 
 	});
+
+
+	$(window).resize(function(){
+		if ($('nav').css('float') == 'right' ){
+			if ( $('#main_navigation li').css('display') == 'none' ) {
+				$('#main_navigation li').css('display','inline-block');
+							} 
+		}
+	});
+
+
+	
 
 
 
