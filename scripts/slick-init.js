7566ce3b430081f2e8308f6d3e68a705773374cc
Jasper Davis <jasper.nicholas.davis@gmail.com>
2015-04-26 22:23:51 -0400

    Built portfolio from commit 2a2dae5 on branch master

--- scripts/slick-init.js
+++ scripts/slick-init.js
@@ -1,28 +1,46 @@
-$(document).ready(function(){
-          $('.main-slick').slick({
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            fade: true,
-            arrows:true,
-            autoplay: true,
-            autoplaySpeed: 6000,
-            asNavFor: '.slider-nav',  
-            responsive: [
-            {
-              breakpoint:768,
-                settings:{
-                  arrows:false}
-              }
-            ]   
-          });
+'use strict';
 
-          $('.slider-nav').slick({
-            asNavFor: '.main-slick',
-            slidesToShow: 3,
-            slidesToScroll: 1,
-            dots: false,
-            centerMode: true,
-            focusOnSelect: true,
-            arrows:false,
-          });
-});
+(function($) {
+
+  $(document).ready(function(){
+            $('.main-slick').slick({
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              fade: true,
+              arrows:true,
+              autoplay: true,
+              autoplaySpeed: 6000,
+              asNavFor: '.slider-nav',  
+              responsive: [
+              {
+                breakpoint:768,
+                  settings:{
+                    arrows:false}
+                }
+              ]   
+            });
+
+            $('.nav01').slick({
+              asNavFor: '.slick01',
+              slidesToShow: 3,
+              slidesToScroll: 1,
+              dots: false,
+              centerMode: true,
+              focusOnSelect: true,
+              arrows:false,
+            });
+
+          
+
+            $('.nav02').slick({
+              asNavFor: '.slick02',
+              slidesToShow: 3,
+              slidesToScroll: 1,
+              dots: false,
+              centerMode: true,
+              focusOnSelect: true,
+              arrows:false,
+            });
+  });
+ 
+})(jQuery);
